4edaddfe51e14ecb74fac41821cbe9481b746d9a
Scr4pe <83241458+Scr4pe@users.noreply.github.com>
2021-07-13 00:02:08 +0200

    measure time of algo and rework of print out

--- code.py
+++ code.py
@@ -1,30 +1,45 @@
 import time
 import random
 
-confirmation_text = "Do you want to start this script:\n[y or Y] + Enter"
-welcome = "\n\nWelcome to Algorithm performance."
-P_create = "Create Random array"
+# strings
+confirmation_text = "\n\nDo you want to start this script:\n[y or Y] + Enter"
+welcome = "\n\nWelcome to Algorithm performance.\n\n"
+P_create = "Create Random list"
 P_start_ = "Starting Algorithm"
 P_finish_selection = ""
 P_exit = "\n\nThank you for using this product."
-global counter
-counter = 0
+P_name = ""
+P_Algo_init = " Algo has started"
+P_Algo_finished = " Algo is finished"
+
+# float
+bub_diff = 0.0
+ins_diff = 0.0
+mer_diff = 0.0
+qui_diff = 0.0
+hea_diff = 0.0
+cou_diff = 0.0
+rad_diff = 0.0
+buc_diff = 0.0
+
+# lists
 rank = []
 random_list = []
 sorted_list = []
 shuffled_list = []
+
+
 # Standard algo blueprint
 # 1. print(started)
 # 2. sort by algorithm
 # 3. Check if the sorted list is correct
 # 3.1 If everything is correct print("Everything is correct")
 # 3.2 If its not correct print(Error in Algo X\n This algo doesnt work)
-# 3.2.1 break up this Algo
 # 4. print("This Algo X is done (1/8)")
 
 
-# Create array
-for i in range(1,5):
+# Create list and shuffle each other on the same way, so every algo has the same shuffled list 
+for i in range(1,100):
     random_list.append(i)
     sorted_list.append(i)
     shuffled_list.append(i)
@@ -32,16 +47,33 @@ shuffle_list = list(zip(random_list,shuffled_list))
 random.shuffle(shuffle_list)
 random_list,shuffled_list = zip(*shuffle_list)
 
+
 # Selection
-def selection_algo():
-    print()
-    # check if the algo worked
+def selection_algo(random_list):
+    # set difference in time later as float to global
+    global sel_diff
+    P_name = "Selection"
+    # create a border
+    print(len(P_name + P_Algo_init) * "#")
+    print(P_name + P_Algo_init)
+    start = time.time()
+    # Algorithm sorting area
+    # >>
+    random_list = sorted(random_list)
+    # >>
+    end = time.time()
+    sel_diff = round(end - start,ndigits=4)
+    print(P_name + P_Algo_finished)
+    # check if the result is correct
     if sorted_list == random_list:
-        print("Algo worked")
+        print("Check was successful")
     else:
-        print("Algo didnt worked")
-    # reset random_list
+        print("Check was unsuccessful")
+    print("reset random_list")
+    # reset unshuffled list
     random_list = shuffled_list
+    # create a border
+    print(len(P_name + P_Algo_init) * "#")
 
 # Bubble
 def bubble_algo():
@@ -77,17 +109,19 @@ def bucket_algo():
 
 # Main function
 def main():
-    confirmation = input("\n\nDo you want to execute this script? \n[Y/y] ")
+    print(confirmation_text)
+    confirmation = input()
     if confirmation == "y" or confirmation == "Y":
         print(welcome)
-        selection_algo()
+        print(P_create)
+        selection_algo(random_list)
+        print("---seconds %s---"%(sel_diff))
 
         # Ranking all Algos
         # Name of Algo, 1. Time, 2. Comparisons, 3. Accesses
 
         # After all algos
         print(P_exit)
-
     else:
         print(P_exit)
 
